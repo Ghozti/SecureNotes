e96b03e515129246d12dbd22607179721d30064b
Ghozti <ybalderas1005@gmail.com>
2021-02-26 13:27:39 -0500

    allowed the note pad to read and display .txt files

--- src/securenotes/mainscreen/NotePad.java
+++ src/securenotes/mainscreen/NotePad.java
@@ -5,6 +5,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.ScrollBar;
 import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.Files;
+import java.util.Scanner;
 
 public class NotePad {
 
@@ -18,8 +24,7 @@ public class NotePad {
     TextArea noteArea;
 
     public String getTextToString(){
-        String s = noteArea.getText();
-        return s;
+        return noteArea.getText();
     }
 
     public void saveFile(ActionEvent actionEvent) {
@@ -30,5 +35,20 @@ public class NotePad {
     }
 
     public void openFile(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        File selectedFile = fileChooser.showOpenDialog(null);
+
+        try {
+            if (selectedFile != null) {
+                Scanner reader = new Scanner(selectedFile);
+                while (reader.hasNextLine()) {
+                    noteArea.setText(reader.nextLine());
+                }
+                reader.close();
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 }
